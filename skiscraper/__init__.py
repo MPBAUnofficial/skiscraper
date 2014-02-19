b889bad4c65d6d162c80e9912cdefc3a6227424f
Marco DallaG <marco.dallagiacoma@gmail.com>
2014-02-19 15:26:53 +0100

    added SkiScraper helper class

--- skiscraper/__init__.py
+++ skiscraper/__init__.py
@@ -3,4 +3,13 @@ from skicenters.latemar import Latemar
 from skicenters.sanmartino import SanMartino
 from skicenters.pinzolo import Pinzolo
 
-__all__ = ['Cermis', 'Latemar', 'SanMartino', 'Pinzolo']
+__all__ = ['SkiScraper']
+
+
+class SkiScraper(object):
+    def __init__(self):
+        self.skicenters = dict(cermis=Cermis(),latemar=Latemar(),
+                               san_martino=SanMartino(), pinzolo=Pinzolo())
+
+    def __getitem__(self, item):
+        return self.skicenters[item]
